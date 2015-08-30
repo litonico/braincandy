ca32d4d9790c5f5a8d6f2190ace1d67affe04ac2
Lito Nicolai <lito.nicolai@gmail.com>
2015-08-30 12:20:58 -0700

    calculate LOTS of pi

---
+++ monte-carlo-pi/makepi.rb
@@ -0,0 +1,32 @@
+class PiEstimator
+  attr_accessor :inside, :outside
+  def initialize
+    self.inside = 0
+    self.outside = 0
+  end
+
+  def shoot
+    [rand, rand]
+  end
+
+  def is_inside? point
+    x, y = point
+    Math.sqrt(x**2 + y**2) < 1
+  end
+
+  def estimate
+    unless outside == 0
+      inside.to_f/outside
+    else
+      0
+    end
+  end
+
+  def refine_estimate
+    if is_inside? shoot
+      self.inside += 1
+    else
+      self.outside += 1
+    end
+  end
+end
